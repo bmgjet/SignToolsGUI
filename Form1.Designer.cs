72418e6479fe1d80681c7e4da627a23eecea051a
bmg <bmgjet@gmail.com>
2021-08-23 22:02:05 +1200

    GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace SignToolsGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ImagePreview = new System.Windows.Forms.PictureBox();
+            this.AddImage = new System.Windows.Forms.Button();
+            this.OpenMap = new System.Windows.Forms.Button();
+            this.mapdir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Locations = new System.Windows.Forms.ComboBox();
+            this.SaveMap = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SignType = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagePreview
+            // 
+            this.ImagePreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImagePreview.Location = new System.Drawing.Point(12, 129);
+            this.ImagePreview.Name = "ImagePreview";
+            this.ImagePreview.Size = new System.Drawing.Size(298, 151);
+            this.ImagePreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagePreview.TabIndex = 0;
+            this.ImagePreview.TabStop = false;
+            // 
+            // AddImage
+            // 
+            this.AddImage.Enabled = false;
+            this.AddImage.Location = new System.Drawing.Point(210, 100);
+            this.AddImage.Name = "AddImage";
+            this.AddImage.Size = new System.Drawing.Size(100, 23);
+            this.AddImage.TabIndex = 1;
+            this.AddImage.Text = "Add Image";
+            this.AddImage.UseVisualStyleBackColor = true;
+            this.AddImage.Click += new System.EventHandler(this.AddImage_Click);
+            // 
+            // OpenMap
+            // 
+            this.OpenMap.Location = new System.Drawing.Point(104, 45);
+            this.OpenMap.Name = "OpenMap";
+            this.OpenMap.Size = new System.Drawing.Size(100, 25);
+            this.OpenMap.TabIndex = 2;
+            this.OpenMap.Text = "Open Map";
+            this.OpenMap.UseVisualStyleBackColor = true;
+            this.OpenMap.Click += new System.EventHandler(this.OpenMap_Click);
+            // 
+            // mapdir
+            // 
+            this.mapdir.Enabled = false;
+            this.mapdir.Location = new System.Drawing.Point(12, 19);
+            this.mapdir.Name = "mapdir";
+            this.mapdir.Size = new System.Drawing.Size(298, 20);
+            this.mapdir.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Map:";
+            // 
+            // Locations
+            // 
+            this.Locations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Locations.Enabled = false;
+            this.Locations.FormattingEnabled = true;
+            this.Locations.Location = new System.Drawing.Point(12, 73);
+            this.Locations.Name = "Locations";
+            this.Locations.Size = new System.Drawing.Size(298, 21);
+            this.Locations.TabIndex = 5;
+            this.Locations.SelectedIndexChanged += new System.EventHandler(this.Selection);
+            // 
+            // SaveMap
+            // 
+            this.SaveMap.Enabled = false;
+            this.SaveMap.Location = new System.Drawing.Point(210, 45);
+            this.SaveMap.Name = "SaveMap";
+            this.SaveMap.Size = new System.Drawing.Size(100, 25);
+            this.SaveMap.TabIndex = 7;
+            this.SaveMap.Text = "Save Map";
+            this.SaveMap.UseVisualStyleBackColor = true;
+            this.SaveMap.Click += new System.EventHandler(this.SaveMap_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Positions:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sign:";
+            // 
+            // SignType
+            // 
+            this.SignType.AutoSize = true;
+            this.SignType.Location = new System.Drawing.Point(46, 110);
+            this.SignType.Name = "SignType";
+            this.SignType.Size = new System.Drawing.Size(0, 13);
+            this.SignType.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 641);
+            this.Controls.Add(this.SignType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SaveMap);
+            this.Controls.Add(this.Locations);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mapdir);
+            this.Controls.Add(this.OpenMap);
+            this.Controls.Add(this.AddImage);
+            this.Controls.Add(this.ImagePreview);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SignToolsGUI 001";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImagePreview;
+        private System.Windows.Forms.Button AddImage;
+        private System.Windows.Forms.Button OpenMap;
+        private System.Windows.Forms.TextBox mapdir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Locations;
+        private System.Windows.Forms.Button SaveMap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label SignType;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using LZ4;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SignToolsGUI
+{
+    public partial class Form1 : Form
+    {
+        WorldSerialization worldSerialization = new WorldSerialization();
+        uint Version;
+        Dictionary<string, byte[]> SerializedImageData = new Dictionary<string, byte[]>();
+        Dictionary<string, byte[]> ModdedSerializedImageData = new Dictionary<string, byte[]>();
+        uint[] signids = { 1447270506, 4057957010, 120534793, 58270319, 4290170446, 3188315846, 3215377795, 1960724311, 3159642196, 3725754530, 1957158128, 637495597, 1283107100, 4006597758, 3715545584, 3479792512, 3618197174, 550204242 };
+        private Dictionary<string, SignSize> _signSizes = new Dictionary<string, SignSize>
+        {
+            {"4006597758", new SignSize(512, 512)},
+            {"3215377795", new SignSize(256, 128)},
+            {"3159642196", new SignSize(128, 512)},
+            {"1960724311", new SignSize(128, 256)},
+            {"3725754530", new SignSize(1024, 512)},
+            {"1957158128", new SignSize(512, 512)},
+            {"1447270506", new SignSize(128, 64)},
+            {"3715545584", new SignSize(256, 128)},
+            {"3479792512", new SignSize(256, 128)},
+            {"3618197174", new SignSize(512, 128)},
+            {"637495597", new SignSize(64, 256)},
+            {"3188315846", new SignSize(64, 256)},
+            {"58270319", new SignSize(128, 64)},
+            {"4290170446", new SignSize(256, 256)},
+            {"120534793", new SignSize(256, 128)},
+            {"4057957010", new SignSize(256, 128)},
+            {"550204242", new SignSize(128, 256)},
+            {"1283107100", new SignSize(256, 128)},
+        };
+        private class SignSize
+        {
+            public int Width;
+            public int Height;
+            public SignSize(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        void XMLDecode(string SerialData)
+        {
+            string[] DataParse = SerialData.Split(new string[] { "<position>" }, StringSplitOptions.None);
+            foreach (string xmldata in DataParse)
+            {
+                if (xmldata.Contains("xml version")) continue;
+                string x = xmldata.Split(new string[] { "</x><y>" }, StringSplitOptions.None)[0].Replace("<x>", "");
+                string y = xmldata.Split(new string[] { "</y><z>" }, StringSplitOptions.None)[0].Replace("<x>" + x + "</x><y>", "");
+                string z = xmldata.Split(new string[] { "</z></position>" }, StringSplitOptions.None)[0].Replace("<x>" + x + "</x><y>" + y + "</y><z>", "");
+                string texture = xmldata.Split(new string[] { "<texture>" }, StringSplitOptions.None)[1].Replace("</texture>", "").Replace("</SerializedImageData>", "");
+                byte[] ImageData = Convert.FromBase64String(texture);
+                SerializedImageData.Add("("+ x+", "+y + ", "+z+")", ImageData);
+            }
+        }
+
+        string XMLEncode()
+        {
+            string XMLData = @"<? xml version=""1.0""?><SerializedImageData>";
+            string SerialData = "";
+            foreach (KeyValuePair<string, byte[]> _sign in ModdedSerializedImageData)
+            {
+                if (_sign.Value.Length != 0)
+                {
+                    string[] xmlbreakdown = _sign.Key.Replace("(", "").Replace(" ", "").Replace(")", "").Split(',');
+
+
+                    SerialData += ("<position>" +
+                                   "<x>" + xmlbreakdown[0] + "</x>" +
+                                   "<y>" + xmlbreakdown[1] + "</y>" +
+                                   "<z>" + xmlbreakdown[2] + "</z>" +
+                                   "</position>" +
+                                   "<texture>" +
+                                   Convert.ToBase64String(_sign.Value) +
+                                   "</texture>");
+                }
+            }
+            XMLData = XMLData + SerialData + "</SerializedImageData>";
+            return XMLData;
+        }
+
+        private void DisableButtons()
+        {
+            SaveMap.Enabled = false;
+            Locations.Enabled = false;
+            AddImage.Enabled = false;
+            ImagePreview.Enabled = false;
+        }
+        private bool isSign(PrefabData sign)
+        {
+            //Checks prefab has a valid sign id
+            return (signids.Contains(sign.id));
+        }
+
+       
+        byte[] ImageResize(byte[] imageBytes, int width, int height)
+        {
+            //Resize image to sign size.
+            Bitmap resizedImage = new Bitmap(width, height),
+                sourceImage = new Bitmap(new MemoryStream(imageBytes));
+
+            Graphics.FromImage(resizedImage).DrawImage(sourceImage, new Rectangle(0, 0, width, height),
+                new Rectangle(0, 0, sourceImage.Width, sourceImage.Height), GraphicsUnit.Pixel);
+
+            var ms = new MemoryStream();
+            resizedImage.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
+        }
+        private void OpenMap_Click(object sender, EventArgs e)
+        {
+            Locations.Items.Clear();
+            SerializedImageData.Clear();
+            ModdedSerializedImageData.Clear();
+                
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Rust Map File",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "map",
+                Filter = "map files (*.map)|*.map",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                mapdir.Text = openFileDialog1.FileName;
+                try
+                {
+                    worldSerialization.Load(mapdir.Text);
+                    Version = worldSerialization.Version;
+
+                //Check for exsisting
+                for (int i = worldSerialization.world.maps.Count - 1; i >= 0; i--)
+                {
+                    MapData mapdata = worldSerialization.world.maps[i];
+                    if (mapdata.name == Base64Encode("SerializedImageData"))
+                    {
+                        XMLDecode(System.Text.Encoding.ASCII.GetString(mapdata.data));
+                    }
+                }
+
+                //Scan all prefab in map file.
+                for (int i = worldSerialization.world.prefabs.Count - 1; i >= 0; i--)
+                {
+                    PrefabData prefabdata = worldSerialization.world.prefabs[i];
+                    if (isSign(prefabdata))
+                    {
+                        string location = "(" + prefabdata.position.x.ToString("0.0") + ", " + prefabdata.position.y.ToString("0.0") + ", " + prefabdata.position.z.ToString("0.0") + ")";
+                        Locations.Items.Add(location + " " + prefabdata.id.ToString());
+
+                        if(SerializedImageData.ContainsKey(location))
+                        {
+                            ModdedSerializedImageData.Add(location, SerializedImageData[location]);
+                        }
+                        else
+                        {
+                            ModdedSerializedImageData.Add(location, new byte[0]);
+                        }
+                    }
+                }
+                if(Locations.Items.Count == 0)
+                {
+                    MessageBox.Show("No Signs are on this map");
+                    return;
+                }
+                    Locations.Text = Locations.Items[0].ToString();
+
+                    SaveMap.Enabled = true;
+                    Locations.Enabled = true;
+                    AddImage.Enabled = true;
+                    ImagePreview.Enabled = true;
+                    SignType.Enabled = true;
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to open map");
+                    DisableButtons();
+                }
+            }
+        }
+
+        private void Selection(object sender, EventArgs e)
+        {
+            string[] selected = Locations.GetItemText(Locations.SelectedItem).Split(')');
+            SignType.Text = selected[1].Replace(" ", "");
+            Size size = new Size(_signSizes[SignType.Text].Width, _signSizes[SignType.Text].Height);
+            ImagePreview.Size = size;
+            if (ModdedSerializedImageData.ContainsKey(selected[0]+")"))
+            {
+                if (ModdedSerializedImageData[selected[0] + ")"].Length != 0)
+                {
+                    ImagePreview.Image = ByteToImage(ModdedSerializedImageData[selected[0] + ")"]);
+                }
+                else
+                {
+                    ImagePreview.Image = null;
+                }
+            }
+        }
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
+        }
+
+        private void AddImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Image File",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "png",
+                Filter = "png files (*.png)|*.png",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                byte[] NewImage = ImageResize(File.ReadAllBytes(openFileDialog1.FileName), ImagePreview.Width, ImagePreview.Height);
+                string[] selected = Locations.GetItemText(Locations.SelectedItem).Split(')');
+                if (ModdedSerializedImageData.ContainsKey(selected[0] + ")"))
+                {
+                    ModdedSerializedImageData[selected[0] + ")"] = NewImage;
+                    ImagePreview.Image = ByteToImage(ModdedSerializedImageData[selected[0] + ")"]);
+                }
+            }
+        }
+
+        private void SaveMap_Click(object sender, EventArgs e)
+        {
+            string XMLData = XMLEncode();
+            //Check if mapdata already has image data
+            MapData sd = worldSerialization.GetMap(Base64Encode("SerializedImageData"));
+            if (sd == null)
+            {
+                worldSerialization.AddMap(Base64Encode("SerializedImageData"), Encoding.ASCII.GetBytes(XMLData));
+            }
+            else
+            {
+                sd.data = Encoding.ASCII.GetBytes(XMLData);
+            }
+            string mapfile = mapdir.Text.Replace(".map",".signs.map");
+            if (File.Exists(mapfile))
+            {
+                File.Delete(mapfile);
+            }
+            using (FileStream fileStream = new FileStream(mapfile, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                {
+                    binaryWriter.Write(Version);
+                    using (LZ4Stream stream = new LZ4Stream(fileStream, LZ4StreamMode.Compress))
+                    {
+                        WorldData.Serialize(stream, worldSerialization.world);
+                    }
+                }
+            }
+            MessageBox.Show("Saved");
+        }
+    }
+}
